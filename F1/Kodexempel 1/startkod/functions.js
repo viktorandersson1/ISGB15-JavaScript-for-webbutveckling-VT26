861fffc99e7c2b312ffb8e9f95e8e37d8f3d41db
Viktor Andersson <lars.viktor.andersson@live.se>
2026-03-04 15:27:44 +0100

    test

---
+++ F1/Kodexempel 1/startkod/functions.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    let name = "Viktor";
+    console.log("Hello " + name);
+
+    function write(name, text){
+document.write("<b>" + name + ": " + text + "</b>")
+    }
+}
